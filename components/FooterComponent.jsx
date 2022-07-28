0756a3d5fadda99938ccda3da445227115f0d0cd
Aniket Chandrapal Dewnani <55101835+Aniket31-coder@users.noreply.github.com>
2022-07-28 13:44:26 +0530

    Made Footer dynamic

--- components/FooterComponent.jsx
+++ components/FooterComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function FooterComponent() {
   return (
     <div className='bg-black text-white py-4  w-full mx-auto text-center'>
-        © Copyrights 2022. All Rights Reserved.
+        © Copyrights {new Date().getFullYear()}. All Rights Reserved.
         <span className='hidden md:inline'> Created with 💖 By Aniket Dewnani</span>
         <span className='inline md:hidden'><br /><br />Created with 💖 By Aniket Dewnani</span>
     </div>
